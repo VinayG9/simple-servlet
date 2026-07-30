96d4e816b6794fc4c85d441a93def47de1ddd188
naga vutukuri <nagavutukuri171@gmail.com>
2026-07-30 20:32:53 +0530

    docker hype-x

--- src/main/java/Counter.java
+++ src/main/java/Counter.java
@@ -2,14 +2,12 @@ package com.myapp;
 import java.io.*;
 import jakarta.servlet.*;
 import jakarta.servlet.http.*;
-
 /*import javax.servlet.*;
-import javax.servlet.http.*;
-
-test*/
-
+import javax.servlet.http.*;*/
+   
 public class Counter extends HttpServlet {
     int accesses = 0;
+     
 
     public void doGet(HttpServletRequest request, HttpServletResponse response)
          throws IOException, ServletException
